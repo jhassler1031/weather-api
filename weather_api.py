c11b4832f2152b2bd61bbcab8797ffac6a650338
jhassler1031 <jhassler1031@gmail.com>
2018-06-04 15:23:00 -0400

    added clothing suggestions

--- weather_api.py
+++ weather_api.py
@@ -18,14 +18,14 @@ else:
 #Show current weather for hometown - Spartanburg
 
 spartanburg_id = "4597204"
-spartanburg_url = f"http://api.openweathermap.org/data/2.5/weather?id={spartanburg_id}&APPID={api_key}"
+spartanburg_url = f"http://api.openweathermap.org/data/2.5/weather?id={spartanburg_id}&APPID={api_key}&units=imperial"
 resp = requests.get(spartanburg_url)
 
 if resp.status_code in [200, 201]:
     weather_data = resp.json()
     print(f"""
     The current weather in {weather_data["name"]} is: {weather_data["weather"][0]["description"]}
-    Current temp (in degrees Kelvin): {weather_data["main"]["temp"]}
+    Current temp (in Fahrenheit): {weather_data["main"]["temp"]}
     Temperature range: High: {weather_data["main"]["temp_max"]} Low: {weather_data["main"]["temp_min"]}
     Humidity: {weather_data["main"]["humidity"]}
     """)
@@ -35,7 +35,7 @@ else:
 #Show 5 day forecast for vacation spot - Berlin
 
 berlin_id = "6545310"
-berlin_url_forecast = f"http://api.openweathermap.org/data/2.5/forecast?id={berlin_id}&APPID={api_key}"
+berlin_url_forecast = f"http://api.openweathermap.org/data/2.5/forecast?id={berlin_id}&APPID={api_key}&units=imperial"
 resp = requests.get(berlin_url_forecast)
 
 if resp.status_code in [200, 201]:
@@ -45,10 +45,22 @@ if resp.status_code in [200, 201]:
         dt_obj = datetime.strptime(item["dt_txt"], "%Y-%m-%d %H:%M:%S")
         if dt_obj.hour == 12:
             print(f"""
-            On {str(dt_obj.month)}, {str(dt_obj.day)} it will be {item["weather"][0]["description"]}
-            Temperature (in Kelvin): {item["main"]["temp"]}
-            Temp range: High: {item["main"]["temp_max"]} Low: {item["main"]["temp_min"]}
-            Humidity: {item["main"]["humidity"]}
+On {str(dt_obj.month)}, {str(dt_obj.day)} it will be {item["weather"][0]["description"]}
+Temperature (in Fahrenheit): {item["main"]["temp"]}
+Temp range: High: {item["main"]["temp_max"]} Low: {item["main"]["temp_min"]}
+Humidity: {item["main"]["humidity"]}
             """)
+            if item["weather"][0]["id"] >= 200 and item["weather"][0]["id"] < 600:
+                print("You may want to bring an umbrella.")
+            if item["main"]["temp"] > 70 and item["main"]["temp"] < 110:
+                print("Break out the shorts!")
+            elif item["main"]["temp"] < 70 and item["main"]["temp"] > 50:
+                print("Jeans and maybe a light jacket.")
+            elif item["main"]["temp"] < 50 and item["main"]["temp"] > 32:
+                print("Definitely want a coat.")
+            elif item["main"]["temp"] < 32:
+                print("Bundle up, it's cold!")
+            elif item["main"]["temp"] > 110:
+                print("Wear SPF 1000!")
 else:
     print("ERROR: " + str(resp.status_code))
