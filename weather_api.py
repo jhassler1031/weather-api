ed4e0d54f71e055f1009df6b2fadfa9170d1b051
jhassler1031 <jhassler1031@gmail.com>
2018-06-04 13:00:03 -0400

    now working for moscow

--- weather_api.py
+++ weather_api.py
@@ -6,8 +6,7 @@ test_url = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + a
 resp = requests.get(test_url)
 
 if resp.status_code in [200, 201]:
-   weather_data = resp.json()
-   print("The weather in {} is {}.".format(weather_data['name'],
-                                           weather_data['weather'][0]['description'])
+    weather_data = resp.json()
+    print("The weather in {} is {}.".format(weather_data['name'], weather_data['weather'][0]['description']))
 else:
-   print("ERROR: " + str(resp.status_code))
+    print("ERROR: " + str(resp.status_code))
